8a451610b06bd82c02cdcd6fc3b73ce796f491c4
LinCie <aldiofernanda@protonmail.com>
2026-03-28 19:11:45 +0000

    feat: add metadatas

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,30 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import swup from '@swup/astro';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  integrations: [
-    swup({
-      theme: false,
-      animationClass: 'transition-',
-      containers: ['main'],
-      cache: true,
-      preload: { hover: true, visible: false },
-      accessibility: true,
-      smoothScrolling: true,
-      morph: ['nav'],
-      updateHead: true,
-      reloadScripts: false,
-      globalInstance: false,
-    })
-  ]
+	site: 'https://lincie.me',
+	integrations: [
+		swup({
+			theme: false,
+			animationClass: 'transition-',
+			containers: ['main'],
+			cache: true,
+			preload: { hover: true, visible: false },
+			accessibility: true,
+			smoothScrolling: true,
+			morph: ['nav'],
+			updateHead: true,
+			reloadScripts: false,
+			globalInstance: false,
+		}),
+		sitemap({
+			filter: (page) => !page.includes('/diary') && !page.includes('/404'),
+		}),
+	],
+	vite: {
+		plugins: [tailwindcss()]
+	},
 });
