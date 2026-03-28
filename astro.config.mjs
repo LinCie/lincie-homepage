0536f17c662c950914a1c3f7113ad91d2517462a
LinCie <aldiofernanda@protonmail.com>
2026-03-28 18:05:06 +0000

    feat: add swup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,26 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import swup from '@swup/astro';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [
+    swup({
+      theme: false,
+      animationClass: 'transition-',
+      containers: ['main'],
+      cache: true,
+      preload: { hover: true, visible: false },
+      accessibility: true,
+      smoothScrolling: true,
+      morph: ['nav'],
+      updateHead: true,
+      reloadScripts: false,
+      globalInstance: false,
+    })
+  ]
 });
